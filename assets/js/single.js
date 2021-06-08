16010e0de54610ebe496b4862e727a0e15725bb3
Ryely <ryely.moore@outlook.com>
2021-06-07 23:33:50 -0600

    adding linking from index page to single-repo

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,7 @@
 var issueContainerEl = document.querySelector("#issues-container");
 
+var limitWarningEl = document.querySelector("#limit-warning");
+
 var getRepoIssues = function(repo) {
     var apiURL = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
@@ -9,6 +11,11 @@ var getRepoIssues = function(repo) {
             response.json().then(function(data){
                 // pass response data to dom function
                 displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         } else {
             alert("There was a problem with your request");
@@ -54,4 +61,13 @@ var displayIssues = function(issues) {
 
 }
 
-getRepoIssues("facebook/react");
+var displayWarning = function(repo) {
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+    // add text warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit";
+}
+
+getRepoIssues("expressjs/express");
